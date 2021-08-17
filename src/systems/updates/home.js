f7e33abdaec049cb3edf3e23c0d70144ee13d4e8
Quinten Clause <quintenclause@hotmail.com>
2021-08-17 23:56:12 +0200

    Change smooting a bit

--- src/systems/updates/home.js
+++ src/systems/updates/home.js
@@ -1,7 +1,7 @@
 let update = (entities, entity, time, delta) => {
     if (entity.position !== undefined) {
         entity.position.x = entity.position.x
-            + (entity.home.x - entity.position.x) / 2 * delta / 17;
+            + (entity.home.x - entity.position.x) / 4 * delta / 17;
         if (
             Math.abs(entity.home.x - entity.position.x)
                 < 1 / entities.game.canvas.zoom
@@ -9,7 +9,7 @@ let update = (entities, entity, time, delta) => {
             entity.position.x = entity.home.x;
         }
         entity.position.y = entity.position.y
-            + (entity.home.y - entity.position.y) / 2 * delta / 17;
+            + (entity.home.y - entity.position.y) / 4 * delta / 17;
         if (
             Math.abs(entity.home.y - entity.position.y)
                 < 1 / entities.game.canvas.zoom
