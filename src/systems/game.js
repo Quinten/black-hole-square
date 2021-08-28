763967099a89be6c1709ee7bbae3e7ea8b03cdba
Quinten Clause <quintenclause@hotmail.com>
2021-08-28 20:31:31 +0200

    Tweak resizing again
    (still attempting to make it work exactly how i want it in ios safari)

--- src/systems/game.js
+++ src/systems/game.js
@@ -16,32 +16,22 @@ if (document.monetization) {
 let setup = (entities) => {
     let game = entities.game;
     let ctx;
-    if (game.canvas !== undefined) {
-        let canvas = document.createElement('canvas');
-        ctx = canvas.getContext('2d');
-        let parent = document.querySelector(
-            game.canvas.parentSelector
-        );
-        if (game.canvas.parentSelector === 'body') {
-            resizeSystem.add(game, canvas);
-        } else {
-            canvas.width = game.canvas.w;
-            canvas.height = game.canvas.h;
-        }
-        parent.appendChild(canvas);
-        if (game.pointer !== undefined) {
-            pointerSystem.add(game, canvas);
-        }
-    }
-    if (game.statemachine === undefined || ctx === undefined) {
-        return;
-    }
+    let canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
+    let parent = document.querySelector(
+        game.canvas.parentSelector
+    );
+    resizeSystem.resize(game, canvas);
+    parent.appendChild(canvas);
+    pointerSystem.add(game, canvas);
     let timer = 0;
     let delta = 0;
     let onF = time => {
         delta = time - timer;
         timer = time;
         //console.log(delta);
+
+        resizeSystem.resize(game, canvas);
 
         let updates = [];
         let draws = [];
@@ -58,10 +48,8 @@ let setup = (entities) => {
         updateSystem.process(entities, updates, time, delta);
 
         // reset pointer
-        if (game.pointer !== undefined) {
-            game.pointer.justDown = false;
-            game.pointer.justUp = false;
-        }
+        game.pointer.justDown = false;
+        game.pointer.justUp = false;
 
         // clear canvas
         ctx.clearRect(0, 0, game.canvas.w, game.canvas.h);
--- src/systems/resize.js
+++ src/systems/resize.js
@@ -1,40 +1,39 @@
-let add = (game, canvas) => {
-    if (game.canvas === undefined) {
-        return game;
+let targetRatio = 1;
+let actualRatio = 1;
+let offsetX = 0;
+let offsetY = 0;
+let w = 0;
+let h = 0;
+
+let resize = (game, canvas) => {
+    if (w === window.innerWidth && h === window.innerHeight) {
+        return;
     }
-    let resizeTOID = 0;
-    let targetRatio = 1;
-    let actualRatio = 1;
-    let onR = e => {
-        clearTimeout(resizeTOID);
-        resizeTOID = setTimeout(_ => {
-            canvas.style.width = window.innerWidth + 'px';
-            canvas.style.height = window.innerHeight + 'px';
-            canvas.width = window.innerWidth * window.devicePixelRatio;
-            canvas.height = window.innerHeight * window.devicePixelRatio;
-            game.canvas.w = canvas.width;
-            game.canvas.h = canvas.height;
-            targetRatio = game.canvas.tW / game.canvas.tH;
-            actualRatio = game.canvas.w / game.canvas.h;
-            if (targetRatio < actualRatio) {
-                game.canvas.zoom = game.canvas.h / game.canvas.tH;
-                game.canvas.gH = game.canvas.tH;
-                game.canvas.gW = game.canvas.tH * game.canvas.w / game.canvas.h;
-            } else {
-                game.canvas.zoom = game.canvas.w / game.canvas.tW;
-                game.canvas.gW = game.canvas.tW;
-                game.canvas.gH = game.canvas.tW * game.canvas.h / game.canvas.w;
-            }
-            let offsetX = (game.canvas.gW - game.canvas.tW) / 2;
-            let offsetY = (game.canvas.gH - game.canvas.tH) / 2;
-            game.canvas.gX = offsetX;
-            game.canvas.gY = offsetY;
-        }, 40);
-    };
-    onR();
-    window.addEventListener('resize', onR);
+    w = window.innerWidth;
+    h = window.innerHeight;
+    canvas.style.width = w + 'px';
+    canvas.style.height = h + 'px';
+    canvas.width = w * window.devicePixelRatio;
+    canvas.height = h * window.devicePixelRatio;
+    game.canvas.w = canvas.width;
+    game.canvas.h = canvas.height;
+    targetRatio = game.canvas.tW / game.canvas.tH;
+    actualRatio = game.canvas.w / game.canvas.h;
+    if (targetRatio < actualRatio) {
+        game.canvas.zoom = game.canvas.h / game.canvas.tH;
+        game.canvas.gH = game.canvas.tH;
+        game.canvas.gW = game.canvas.tH * game.canvas.w / game.canvas.h;
+    } else {
+        game.canvas.zoom = game.canvas.w / game.canvas.tW;
+        game.canvas.gW = game.canvas.tW;
+        game.canvas.gH = game.canvas.tW * game.canvas.h / game.canvas.w;
+    }
+    offsetX = (game.canvas.gW - game.canvas.tW) / 2;
+    offsetY = (game.canvas.gH - game.canvas.tH) / 2;
+    game.canvas.gX = offsetX;
+    game.canvas.gY = offsetY;
 };
 
 export default Object.freeze({
-    add
+    resize
 });
