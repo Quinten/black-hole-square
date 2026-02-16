4a760a0f3d182c833e345937a0fe4b7704afd737
Quinten Clause <quintenclause@hotmail.com>
2026-02-16 07:49:12 +0100

    rm monetisation lock

--- src/systems/game.js
+++ src/systems/game.js
@@ -4,17 +4,14 @@ import resizeSystem from './resize.js';
 import pointerSystem from './pointer.js';
 import dataSystem from './data.js';
 
-/*if (document.monetization) {*/
-    /*document.monetization.addEventListener(*/
-        /*'monetizationstart',*/
-        /*_ => {*/
-            /*dataSystem.save('payed', true);*/
-        /*}*/
-    /*);*/
-/*}*/
-
-// if you pay peanuts, you get monkeys
-dataSystem.save('payed', true);
+if (document.monetization) {
+    document.monetization.addEventListener(
+        'monetizationstart',
+        _ => {
+            dataSystem.save('payed', true);
+        }
+    );
+}
 
 let setup = (entities) => {
     let game = entities.game;
